3dae125376da8cff95ddc51eaea8e7283c1cb604
Phizho <phizzarfernos@gmail.com>
2021-02-17 21:34:01 +0700

    Update css+Layout

---
+++ resources/views/main.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.conquer')
+
+@section('tempat_konten')
+<head>
+</head>
+
+<body>
+    <div>
+        Daftar Surat Keluar Dekan 
+    </div>
+
+    <div></div>
+
+    <table class="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Tanggal</th>
+            <th>No. Surat Keluar Dekan</th>
+            <th>Perihal</th>
+            <th>Tanggal Kirim</th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+            <tbody>
+
+            </tbody>
+    </table>
+
+</body>
+@endsection
